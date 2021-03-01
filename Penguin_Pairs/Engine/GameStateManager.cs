517eebea5e7e1279f6c70e6ddb26f6b8d167d162
CptDads <tjj.jonker@gmail.com>
2021-03-01 20:55:20 +0100

    Created a GameStateManager in the Engine

---
+++ Penguin_Pairs/Engine/GameStateManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Engine
+{
+    internal class GameStateManager
+    {
+        private Dictionary<string, GameState> gameStates;
+        private GameState currentGameState;
+
+        public GameStateManager()
+        {
+            gameStates = new Dictionary<string, GameState>();
+            currentGameState = null;
+        }
+
+        public void AddGameState(string name, GameState state)
+        {
+            gameStates[name] = state;
+        }
+
+        public GameState GetGameState(string name)
+        {
+            if (gameStates.ContainsKey(name))
+                return gameStates[name];
+            return null;
+        }
+
+        public void SwitchTo(string name)
+        {
+            if (gameStates.ContainsKey(name))
+                currentGameState = gameStates[name];
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            currentGameState?.Update(gameTime);
+        }
+    }
+}
