ffb81e06ab66f94a83c359529cca7cda44ac2ec3
CptDads <tjj.jonker@gmail.com>
2021-03-03 13:22:39 +0100

    Created a Button class

--- Penguin_Pairs/Engine/UI/Button.cs
+++ Penguin_Pairs/Engine/UI/Button.cs
@@ -1,6 +1,23 @@
-﻿namespace Penguin_Pairs.Engine.UI
+﻿namespace Engine.UI
 {
-    internal class Button
+    internal class Button : SpriteGameObject
     {
+        public bool Pressed { get; protected set; }
+
+        public Button(string assetName) : base(assetName)
+        {
+            Pressed = false;
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            Pressed = Visible && inputHelper.MouseLeftButtonPressed() && BoundingBox.Contains(inputHelper.MousePositionWorld);
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+            Pressed = false;
+        }
     }
 }
