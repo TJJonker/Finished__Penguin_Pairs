35fffcbe213c043f489177325e71a2a3a8877dc1
CptDads <tjj.jonker@gmail.com>
2021-03-03 22:34:49 +0100

    Added a retry, quit and hint button to the playing state

--- Penguin_Pairs/GameStates/PlayingState.cs
+++ Penguin_Pairs/GameStates/PlayingState.cs
@@ -1,19 +1,44 @@
 ﻿using Engine;
-using Microsoft.Xna.Framework.Input;
+using Engine.UI;
+using Microsoft.Xna.Framework;
 
 namespace Penguin_Pairs
 {
     internal class PlayingState : GameState
     {
+        private Button hintButton, retryButton, quitButton;
+
         public PlayingState()
         {
             SpriteGameObject background = new SpriteGameObject("Sprites/spr_background_level");
             gameObjects.AddChild(background);
+
+            // Adding the hintButton
+            hintButton = new Button("Sprites/UI/spr_button_hint")
+            {
+                Position = new Vector2(916, 20)
+            };
+            gameObjects.AddChild(hintButton);
+
+            // Adding the retryButton
+            retryButton = new Button("Sprites/UI/spr_button_retry")
+            {
+                Position = new Vector2(916, 20),
+                Visible = false
+            };
+            gameObjects.AddChild(retryButton);
+
+            // Adding the quitButton
+            quitButton = new Button("Sprites/UI/spr_button_quit")
+            {
+                Position = new Vector2(1058, 20)
+            };
+            gameObjects.AddChild(quitButton);
         }
 
         public override void HandleInput(InputHelper inputHelper)
         {
-            if (inputHelper.KeyPressed(Keys.Back))
+            if (quitButton.Pressed)
                 ExtendedGame.GameStateManager.SwitchTo(PenguinPairs.StateName_LevelMenu);
         }
     }
