1330c7179e4e47e6e4f4acb03691eabfa08821b1
CptDads <tjj.jonker@gmail.com>
2021-03-03 17:51:52 +0100

    Added Buttons on the title screen which the player can click on to proceed to the next screen

--- Penguin_Pairs/GameStates/TitleMenuState.cs
+++ Penguin_Pairs/GameStates/TitleMenuState.cs
@@ -1,28 +1,47 @@
 ﻿using Engine;
-using Microsoft.Xna.Framework.Input;
+using Engine.UI;
+using Microsoft.Xna.Framework;
 
 namespace Penguin_Pairs
 {
     internal class TitleMenuState : GameState
     {
+        private Button playButton, optionsButton, helpButton;
+
         public TitleMenuState()
         {
             SpriteGameObject titleMenu = new SpriteGameObject("Sprites/spr_titleScreen");
             gameObjects.AddChild(titleMenu);
+
+            // Adding the playButton
+            playButton = new Button("Sprites/UI/spr_button_play");
+            playButton.Position = new Vector2(415, 540);
+            gameObjects.AddChild(playButton);
+
+            // Adding the optionsButton
+            optionsButton = new Button("Sprites/UI/spr_button_options");
+            optionsButton.Position = new Vector2(415, 650);
+            gameObjects.AddChild(optionsButton);
+
+            // Adding the helpButton
+            helpButton = new Button("Sprites/UI/spr_button_help");
+            helpButton.Position = new Vector2(415, 760);
+            gameObjects.AddChild(helpButton);
         }
 
         public override void HandleInput(InputHelper inputHelper)
         {
+            base.HandleInput(inputHelper);
             // Opening Help menu
-            if (inputHelper.KeyPressed(Keys.H))
+            if (helpButton.Pressed)
                 ExtendedGame.GameStateManager.SwitchTo(PenguinPairs.StateName_Help);
 
             // Opening Options menu
-            if (inputHelper.KeyPressed(Keys.O))
+            if (optionsButton.Pressed)
                 ExtendedGame.GameStateManager.SwitchTo(PenguinPairs.StateName_Options);
 
             // Opening Level menu
-            if (inputHelper.KeyPressed(Keys.L))
+            if (playButton.Pressed)
                 ExtendedGame.GameStateManager.SwitchTo(PenguinPairs.StateName_LevelMenu);
         }
     }
