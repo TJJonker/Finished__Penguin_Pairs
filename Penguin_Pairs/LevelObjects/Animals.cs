bbe6e4d76dcb1bae61c69116b22122bf70cc6239
CptDads <tjj.jonker@gmail.com>
2021-03-05 19:05:56 +0100

    Created an Animal class

---
+++ Penguin_Pairs/LevelObjects/Animals.cs
@@ -0,0 +1,11 @@
+﻿using Engine;
+
+namespace Penguin_Pairs.LevelObjects
+{
+    internal abstract class Animal : SpriteGameObject
+    {
+        protected Animal(string spriteName, int sheetIndex = 0) : base(spriteName, sheetIndex)
+        {
+        }
+    }
+}
