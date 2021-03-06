a0154e5dce037a762863447c7979a4dcaa3a9251
CptDads <tjj.jonker@gmail.com>
2021-03-06 20:16:18 +0100

    Created the first version of the Level class which will read level information out of text files

---
+++ Penguin_Pairs/LevelObjects/Level.cs
@@ -0,0 +1,30 @@
+﻿using Engine;
+using Microsoft.Xna.Framework;
+
+namespace Penguin_Pairs.LevelObjects
+{
+    internal class Level : GameObjectList
+    {
+        private const int TileWidth = 73;
+        private const int TileHeight = 72;
+
+        public int LevelIndex { get; private set; }
+        private int targetNuberOfPairs;
+
+        private Tile[,] tiles;
+        private Animal[,] animalsOnTiles;
+
+        private SpriteGameObject hintArrow;
+
+        public Level(int levelIndex, string filename)
+        {
+            LevelIndex = levelIndex;
+            LoadLevelFromFile(filename);
+        }
+
+        private Vector2 GetCellPosition(int x, int y)
+        {
+            return new Vector2(x * TileWidth, y * TileHeight);
+        }
+    }
+}
