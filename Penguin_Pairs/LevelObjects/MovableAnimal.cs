756a72a1490bbeb40ab04db32c96875080c12801
CptDads <tjj.jonker@gmail.com>
2021-03-06 14:02:47 +0100

    Created a MovableAnimal class for the Seals and Penguins. Behaviour is not yet added

---
+++ Penguin_Pairs/LevelObjects/MovableAnimal.cs
@@ -0,0 +1,32 @@
+﻿using Engine;
+namespace Penguin_Pairs
+{
+    internal class MovableAnimal : Animal
+    {
+        private bool isInHole;
+
+        public int AnimalIndex { get { return SheetIndex; } }
+
+        public bool IsInHole
+        {
+            get { return isInHole; }
+            set
+            {
+                isInHole = value;
+                sprite = new SpriteSheet(GetSpriteName(IsInHole), AnimalIndex);
+            }
+        }
+
+        public MovableAnimal(int animalIndex, bool isInHole) : base(GetSpriteName(isInHole), animalIndex)
+        {
+            this.isInHole = isInHole;
+        }
+
+        private static string GetSpriteName(bool isInHole)
+        {
+            if (isInHole)
+                return "Sprites/LevelObjects/spr_penguin_boxed@8";
+            return "Sprites/LevelObjects/spr_penguin@8";
+        }
+    }
+}
